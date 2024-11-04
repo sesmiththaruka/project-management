d19adf4aecc7ed3734ff02403a617fe6687cdf3d
sesmiththaruka <sesmith.tharuka@gmail.com>
2024-11-04 16:20:58 +0530

    server modification

--- server/src/index.ts
+++ server/src/index.ts
@@ -36,6 +36,6 @@ app.get("/", (req, res) => {
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
-app.listen(port, () => {
+app.listen(port,"0.0.0.0", () => {
   console.log(`Server running on part ${port}`);
 });
